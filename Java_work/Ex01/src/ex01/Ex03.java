2edecd24c7bedda900f4be60203f1f9b5af69fcf
DESKTOP-1D3SA0A\user2 <a>
2024-06-28 12:58:42 +0900

    0628

---
+++ Java_work/Ex01/src/ex01/Ex03.java
@@ -0,0 +1,20 @@
+package ex01;
+
+	//ctrl+y 앞으로 가기
+	//ctrl+y 앞으로 가기
+	//ctrl+w  창 닫기
+
+public class Ex03 {
+	public static void main(String[] args) {
+
+		System.out.println("10+3 =" + 10 + 3);
+	//  System.out.println("10+3 ="- 10+3);
+	//  System.out.println String - int
+		System.out.println(50 / 3);
+		System.out.println(3 * 4);
+		System.out.println(10 - 7);
+		System.out.println("10-7 =" + (10 - 7));
+		System.out.println(Math.floor(Math.PI));
+
+	}
+}
