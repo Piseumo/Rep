950ef774a4ae27db901afa7598079995d66dfd3c
DESKTOP-1D3SA0A\user2 <a>
2024-06-28 18:12:10 +0900

    0628

--- Java_work/Ex01/src/aa/Ex08.java
+++ Java_work/Ex01/src/aa/Ex08.java
@@ -2,12 +2,12 @@ package aa;
 
 public class Ex08 {
 	public static void main(String[] args) {
-		//"안녕하세요"
-		//"박명회입니다"
-		//		"개발공부는....."
-		System.out.println("\"안녕하세요\"\n\"박형회입니다\"\n\t\"개발공부는...\"");
-		System.out.println("안녕하세요\n 박명회입니다\n\t 개발공부는....");
-		
-
+		String a = "3"
+				+ "00";
+		System.out.println(a);
+        int b = 100;
+        System.out.println(a+b);
+        System.out.println(Math.sqrt(16));
+        
 	}
 }
