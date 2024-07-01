0324ea530f5c504f8c91fc6fffc9e1e8d0af080f
DESKTOP-1D3SA0A\user2 <a>
2024-07-01 15:05:59 +0900

    no message

--- Java_work/Ex01/src/aa/Ex01.java
+++ Java_work/Ex01/src/aa/Ex01.java
@@ -3,22 +3,11 @@ package aa;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		System.out.println("익숙해지면 좋아요");
 
-		int a = 10;
-		double b = 5.4;
-		b = 10;
-		int c = 3;
-		
-
-		System.out.println(a + b);
-		System.out.println(a / b);
-		System.out.println(a % b);
-
-		System.out.println(a / c);
-		System.out.println(a % c);
-		System.out.println();
-		
+		float d = 1000f;
+		long e = 1234l;
+		 System.out.print(((Object)(d + e)).getClass().getName());
+	
 		
 
 	}
---
+++ Java_work/Ex01/src/ex03/Ex07.java
@@ -0,0 +1,19 @@
+package ex03;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+				
+		System.out.println(a);
+		a += b;
+		System.out.println(a);
+		a += b;
+		a *= b;
+		System.out.println(a);
+		
+		
+
+	}
+
+}
