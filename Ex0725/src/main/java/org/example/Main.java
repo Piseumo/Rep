3eda4891146570b58d1d61ccd7228752ead01d91
Piseumo <tackteho@naver.ccom>
2024-07-29 17:55:06 +0900

    0729

--- Ex0725/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        A a = new A();
-        a = null;
-        System.out.println("Hello");
-    }
-}
--- Java_work/Exercise/src/Example/Ex0725/Ex01.java
+++ Java_work/Exercise/src/Example/Ex0725/Ex01.java
@@ -1,9 +1,7 @@
-package Exercise.src.Example.Ex0725;
+package org.example.Exercise.Ex0725;
 
 public class Ex01 {
     public static void main(String[] args) {
-        A a1 = new A("AAA");
-        A a2 = a1;
 
         System.out.println("");
     }
