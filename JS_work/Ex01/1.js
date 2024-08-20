6d9341b7e1ffc63f138074d8cb34a59819032e44
Pa-fs <qordi124@gmail.com>
2024-08-20 18:12:06 +0900

    0820

---
+++ JS_work/Ex01/1.js
@@ -0,0 +1,15 @@
+var heading = document.querySelector("#heading");
+// alert(heading);
+heading.onclick = function(){
+  heading.style.color = "blue";
+  heading.innerHTML = "바보야";
+  heading.style.backgroundColor="red";
+  heading.style.textAlign="center";
+  heading.style.fontSize="100px";
+  alert('메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱롱메롱메롱메롱메롱메롱메롱메롱메롱메롱메롱');
+};
+btn.onclick =function(){
+  var name = prompt('당신의 이름은?', '이름');
+  alert(name);
+}
+
