924010e8a79b2218735ad1861047708a35552004
DESKTOP-SCJ83VA\user <tackteho@gmail.com>
2024-09-30 18:10:19 +0900

    0930

---
+++ Springboot_work/ex10/src/main/java/com/tth/ex10/user/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.tth.ex10.user;
+
+import com.tth.ex10.error.BizException;
+import com.tth.ex10.error.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public void insert(UserReqDto userReqDto){
+
+        Optional<User> optionalUser = userRepository.findByEmail(userReqDto.getEmail());
+        optionalUser.ifPresent(user -> {
+            throw new BizException(ErrorCode.DUPLICATE_USERS);
+        });
+
+        System.out.println("실행"+ userReqDto);
+        ModelMapper modelMapper = new ModelMapper();
+        User user = modelMapper.map(userReqDto, User.class);
+        System.out.println("user = "+ user);
+        userRepository.save(user);
+
+    }
+
+}
--- Vue_work/ex07/src/router/index.js
+++ Vue_work/ex07/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TheHome from '@/views/TheHome.vue';
-import TheUser from '@/views/users/TheUser.vue';
 import TheUserList from '@/views/users/TheUserList.vue';
 
 import TheFreeBoardInput from '@/views/freeboard/TheFreeBoardInput.vue';
@@ -9,8 +8,6 @@ import TheFreeBoardView from '@/views/freeboard/TheFreeBoardView.vue';
 import TheFreeBoardUpdate from '@/views/freeboard/TheFreeBoardUpdate.vue';
 
 import TheFileUpload from '@/views/TheFileUpload.vue';
-
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,11 +20,6 @@ const router = createRouter({
     {
       path: '/user',
       name: 'user',
-      component: TheUser
-    },
-    {
-      path: '/userlist',
-      name: 'userlist',
       component: TheUserList
     },
     {
