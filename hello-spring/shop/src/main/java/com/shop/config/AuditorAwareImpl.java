ddd3c029d5b6c05e2694c104e712f374d54726ce
DESKTOP-KVLCEEB\user <tackteho@naver.com>
2024-10-24 21:26:05 +0900

    20241024

--- hello-spring/shop/src/main/java/com/shop/config/AuditorAwareImpl.java
+++ hello-spring/shop/src/main/java/com/shop/config/AuditorAwareImpl.java
@@ -1,19 +1,14 @@
 package com.shop.config;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.net.Authenticator;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
-    @NonNull
-    public Optional<String> getCurrentAuditor(){
+    public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = "";
         if(authentication != null){
@@ -21,4 +16,5 @@ public class AuditorAwareImpl implements AuditorAware<String> {
         }
         return Optional.of(userId);
     }
+
 }
